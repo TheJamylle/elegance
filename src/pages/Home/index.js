dece8c225a34f343fbdb202646292779daed37af
TheJamylle <jamyllemagal@gmail.com>
2020-05-23 20:08:32 -0300

    building home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home(){
     return(
         <div className="home-container">
             <nav>
                 <span>El</span>
-                <ul>
-                    <li style="color: #293550">Home</li>
-                    <li>About</li>
-                    <li>Login</li>
-                    <li>Quero Começar</li>
-                </ul>
+                    <Link style="color: #293550">Home</Link>
+                    <Link><a>About</a></Link>
+                    <Link><a>Login</a></Link>
+                    <Link><a>Quero Começar</a></Link>
             </nav>
             <div className="apresentation">
                 o que é Elegance
             </div>
             <div className="details">
-
+                Como funciona
             </div>
         </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, BrowserRouter, HashRouter, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Home from './pages/Home';
 import NewClothe from './pages/NewClothe';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Logon} />
+                <Route path="/" exact component={Home} />
+                <Route path="/login" exact component={Logon} />
                 <Route path="/register" exact component={Register} />
             </Switch>
         </BrowserRouter>
