78da7b235aaff603cc0da60cbee90ccc22c1832a
TheJamylle <jamyllemagal@gmail.com>
2020-05-23 21:02:12 -0300

    estilizando a home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.css';
 
 export default function Home(){
     return(
         <div className="home-container">
             <nav>
-                <span>El</span>
-                    <Link style="color: #293550">Home</Link>
-                    <Link><a>About</a></Link>
-                    <Link><a>Login</a></Link>
-                    <Link><a>Quero Começar</a></Link>
+                <a className="logo">El</a>
+                    <Link><span>Home</span></Link>
+                    <Link><span>About</span></Link>
+                    <Link><span>Login</span></Link>
+                    <Link><span>Quero Começar</span></Link>
             </nav>
             <div className="apresentation">
                 o que é Elegance
             </div>
             <div className="details">
                 Como funciona
+            </div>
+            <div className="contact">
+                Entre em contato
             </div>
         </div>
     );
